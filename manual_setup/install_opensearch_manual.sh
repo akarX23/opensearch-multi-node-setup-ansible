5daf6373dd956572441b83167c7eb0fb745881b5
akarX23 <ritikagrawal1292000@gmail.com>
2023-06-16 20:37:16 +0000

    Fixed bugs

--- manual_setup/install_opensearch_manual.sh
+++ manual_setup/install_opensearch_manual.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+#
 export http_proxy=""
 export https_proxy=""
 export ftp_proxy=""
 export no_proxy="127.0.0.1,localhost"
 
 # For curl
-export HTTP_PROXY=""
-export HTTPS_PROXY=""
-export FTP_PROXY=""
-export NO_PROXY="127.0.0.1,localhost"
+export HTTP_PROXY="$http_proxy"
+export HTTPS_PROXY="$https_proxy"
+export FTP_PROXY="$ftp_proxy"
+export NO_PROXY="$no_proxy"
 
-wget https://artifacts.opensearch.org/releases/bundle/opensearch/2.6.0/opensearch-2.6.0-linux-x64.deb
-sudo dpkg -i opensearch-2.6.0-linux-x64.deb
-#sudo systemctl enable opensearch
-#sudo systemctl start opensearch
+DEB_FILE="opensearch-2.8.0-linux-x64.deb"
 
+if [ ! -f "$DEB_FILE" ]; then
+  wget https://artifacts.opensearch.org/releases/bundle/opensearch/2.8.0/opensearch-2.8.0-linux-x64.deb
+fi
+
+sudo dpkg -i "$DEB_FILE"
